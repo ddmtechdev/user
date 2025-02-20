15d26a469d7c1190c39875343e59b0659a69ea7f
Deniel Maomay <ddmaomay@dilg.gov.ph>
2025-02-20 13:09:57 +0800

    Updated the readme.md and migration

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Installation
+<h4>Installation</h4>
 
 1. php composer require ddmtechdev/user:@dev
-2. Run migration: php yii migrate/up --migrationPath=@vendor/ddmtechdev/yii2-user/migrations
+2. Run migration: php yii migrate/up --migrationPath=@vendor/ddmtechdev/user/migrations
--- migrations/m250220_042300_create_user_table.php
+++ migrations/m250220_042300_create_user_table.php
@@ -14,12 +14,13 @@ class m250220_042300_create_user_table extends Migration
             'username' => $this->string()->notNull()->unique(),
             'email' => $this->string()->notNull()->unique(),
             'password_hash' => $this->string()->notNull(),
-            'auth_key' => $this->string(32),
-            'access_token' => $this->string(255),
+            'auth_key' => $this->string(32)->notNull(), // Ensure it's NOT NULL
+            'access_token' => $this->string(255)->null(),
             'created_at' => $this->timestamp()->defaultExpression('CURRENT_TIMESTAMP'),
             'updated_at' => $this->timestamp()->defaultExpression('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
         ]);
     }
+
 
     public function safeDown()
     {
